e189a51c60627c58c6ee0afaa5b42855f64c701a
MagInventor <programistmag@gmail.com>
2023-03-24 18:45:30 +0300

    refactor: change structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
-
-import { Layout } from './components/Layout'
-import { Homepage } from './pages/Homepage'
-import { Aboutpage } from './pages/Aboutpage'
-import { Formpage } from './pages/Formpage'
-import { Notfoundpage } from './pages/Notfoundpage'
+import Router from './router/router'
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="about" element={<Aboutpage />} />
-          <Route path="form" element={<Formpage />} />
-          <Route path="*" element={<Notfoundpage />} />
-        </Route>
-      </Routes>
+      <Router />
     </div>
   )
 }
